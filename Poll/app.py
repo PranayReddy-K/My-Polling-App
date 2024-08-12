9148778859836104ee0773f4f63db70c86421963
PranayReddy-K <pranay.reddy12@outlook.com>
2024-08-12 16:02:49 +0530

    feat: implemted data-population microservice and worker microservice

--- Poll/app.py
+++ Poll/app.py
@@ -29,12 +29,12 @@ def hello():
 
     vote = None
 
-    # if request.method == 'POST':
-    #     redis = get_redis()
-    #     vote = request.form['vote']
-    #     app.logger.info('Received vote for %s', vote)
-    #     data = json.dumps({'voter_id': voter_id, 'vote': vote})
-    #     redis.rpush('votes', data)
+    if request.method == 'POST':
+        redis = get_redis()
+        vote = request.form['vote']
+        app.logger.info('Received vote for %s', vote)
+        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        redis.rpush('votes', data)
 
     resp = make_response(render_template(
         'index.html',
